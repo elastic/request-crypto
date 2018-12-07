da4c55455fcfd76e7252910d7bcea2eccc87a66d
Chris Earle <pickypg@users.noreply.github.com>
2018-12-08 01:24:26 +0200

    Update src/util.ts
    Co-Authored-By: Bamieh <ahmadbamieh@gmail.com>

--- src/util.ts
+++ src/util.ts
@@ -1,5 +1,5 @@
 export function checkNodeCompatiblity(targetMajorMinor: number): void {
   if (+process.version.match(/v(\d{1,2}.\d{1,2}).\d{1,2}/)![1] < targetMajorMinor) {
-    throw Error('Node.js version must be above v10.12 to run this command.');
+    throw Error(`Node.js version must be v${targetMajorMinor} or newer to run this command.`);
   }
 }
